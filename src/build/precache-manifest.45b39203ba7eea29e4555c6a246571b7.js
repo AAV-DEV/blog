7571ba5cab5dfcb963626b1f4bbec502e774c040
avoitenco <avoitenco@outlook.com>
2020-09-27 14:42:58 -0600

    First commit

---
+++ src/build/precache-manifest.45b39203ba7eea29e4555c6a246571b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb65c75d437f08e465b8d9119888c5e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7669a5057a72b83357a4",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "d7c7ef62f562b707ab01",
+    "url": "/static/js/2.e7e1d73f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e7e1d73f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7669a5057a72b83357a4",
+    "url": "/static/js/main.4059bf23.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
